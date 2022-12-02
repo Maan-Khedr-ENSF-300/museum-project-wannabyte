4cc89c83f926b291f2cbc9495089d763c1e58e2e
Fraserwill03 <williamgfraser2015@gmail.com>
2022-12-02 14:50:29 -0700

    added some more...

--- SQL Scripts/art_museum.sql
+++ SQL Scripts/art_museum.sql
@@ -4,13 +4,36 @@ USE ART_MUSEUM;
 
 DROP TABLE IF EXISTS art_object;
 CREATE TABLE art_object (
-    ID_no               INTEGER,
-    Title               VARCHAR(255),
-    Descrip             VARCHAR(255),
-    year_created        INTEGER,
-    Epoch               VARCHAR(30),
-    Country_of_origin   VARCHAR(30)
-    PRIMARY KEY (ID_no)
+    ID_no               INTEGER NOT NULL,
+    Title               VARCHAR(255) NOT NULL,
+    Descrip             VARCHAR(255) NOT NULL,
+    year_created        VARCHAR(30) DEFAULT 'UNKNOWN',
+    Epoch               VARCHAR(30) NOT NULL,
+    Country_of_origin   VARCHAR(30) NOT NULL,
+    Artist              VARCHAR(30) DEFAULT 'UNKNOWN'
+    PRIMARY KEY (ID_no),
+    FOREIGN KEY (Artist_first) REFERENCES ARTIST(PK_name)
 );
 
+DROP TABLE IF EXISTS painting;
+CREATE TABLE painting (
+    ID_no               INTEGER NOT NULL,
+    Paint_type          VARCHAR(30) NOT NULL,
+    Drawn_on            VARCHAR(30) NOT NULL,
+    Style               VARCHAR(30) NOT NULL
+    PRIMARY KEY (ID_no),
+    FOREIGN KEY (ID_no) REFERENCES art_object(ID_no)    
+);
 
+DROP TABLE IF EXISTS artist;
+CREATE TABLE artist(
+    Fname               VARCHAR(30) NOT NULL,
+    Lname               VARCHAR(30) NOT NULL,
+    Date_born           VARCHAR(30) DEFAULT 'UNKNOWN',
+    Date_died           VARCHAR(30) DEFAULT 'UNKNOWN',
+    Country_of_origin   VARCHAR(30) NOT NULL,
+    Epoch               VARCHAR(30) NOT NULL,
+    Main_stlye          VARCHAR(30) NOT NULL,
+    Descrip             VARCHAR(255) NOT NULL
+    CONSTRAINT PK_name PRIMARY KEY (Fname, Lname);
+);
