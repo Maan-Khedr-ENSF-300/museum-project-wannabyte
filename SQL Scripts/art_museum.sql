db00ac4a43368cd62a0b2ce4bf1c50db21e2fe68
Fraserwill03 <williamgfraser2015@gmail.com>
2022-12-02 14:18:13 -0700

    art_object table creation added

--- SQL Scripts/art_museum.sql
+++ SQL Scripts/art_museum.sql
@@ -1,1 +1,16 @@
+DROP DATABASE IF EXISTS ART_MUSEUM;
+CREATE DATABASE ART_MUSEUM; 
+USE ART_MUSEUM;
+
+DROP TABLE IF EXISTS art_object;
+CREATE TABLE art_object (
+    ID_no               INTEGER,
+    Title               VARCHAR(255),
+    Descrip             VARCHAR(255),
+    year_created        INTEGER,
+    Epoch               VARCHAR(30),
+    Country_of_origin   VARCHAR(30)
+    PRIMARY KEY (ID_no)
+);
+
 
