e489582d2830fcbea720642171372e756931b669
Fraserwill03 <williamgfraser2015@gmail.com>
2022-12-03 15:23:38 -0700

    Began artist table and paintings table

--- SQL Scripts/art_museum_population.sql
+++ SQL Scripts/art_museum_population.sql
@@ -1,1 +1,17 @@
+USE ART_MUSEUM;
+
+INSERT INTO TABLE artist
+VALUES 
+('Bennedeto', 'da Rovezzano', '1474', '1552', 'Italy', 'Early Modern Period', 'Renaissance', 'Bennedeto was an Italian Architect and Sculptor who worked mainly in Florence.')
+('Hans', 'Holbein', '1497', '1543', 'Germany', 'Early Modern Period', 'Northern Renaissance', 'Holbein was a German-Swiss artist who is known as being one fo the best portraitists of the 16th century.')
+('Affabel', 'Partridge', 'UNKNOWN', 'UNKNOWN', 'Britain', 'Early Modern Period', 'Renaissance', 'Partridge was a goldsmith who worked in London, who is known for being tasked with improving old jewlery for Elizabeth I')
+('Earl', 'Robbins', '1922', '2010', 'United States', 'Postmodernity', 'Earthenware', 'Earl Robbins was a Native American artist from South Carolina.')
+('Vincent', 'van Gogh', '1853', '1890', 'Netherlands', 'Long Nineteenth Century', 'Post-Impressionist', 'Vincent Van Gogh was a Dutch painter who has become extremely well known in modern times, but did not receive much credit during his time.')
+('Louis', 'Richard', '1791', '1879', 'France', 'Long Nineteenth Century', 'Bronze Sculpting', 'Richard was a 19th century French scultor, known for creating realistic statues.')
+
+INSERT INTO TABLE painting
+VALUES
+('003', 'Oil', 'Panel', 'Renaissance')
+('007', 'Oil', 'Canvas', 'Post-Impressionist')
+('006', 'Print', 'Transfer Lithograph', 'Post-Impressionist')
 
