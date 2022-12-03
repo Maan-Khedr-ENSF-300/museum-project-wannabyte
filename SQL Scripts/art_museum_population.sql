6757924163633b921f0a46766080fa1bf38258ea
Fraserwill03 <williamgfraser2015@gmail.com>
2022-12-03 15:33:27 -0700

    Added more

--- SQL Scripts/art_museum_population.sql
+++ SQL Scripts/art_museum_population.sql
@@ -7,11 +7,25 @@ VALUES
 ('Affabel', 'Partridge', 'UNKNOWN', 'UNKNOWN', 'Britain', 'Early Modern Period', 'Renaissance', 'Partridge was a goldsmith who worked in London, who is known for being tasked with improving old jewlery for Elizabeth I')
 ('Earl', 'Robbins', '1922', '2010', 'United States', 'Postmodernity', 'Earthenware', 'Earl Robbins was a Native American artist from South Carolina.')
 ('Vincent', 'van Gogh', '1853', '1890', 'Netherlands', 'Long Nineteenth Century', 'Post-Impressionist', 'Vincent Van Gogh was a Dutch painter who has become extremely well known in modern times, but did not receive much credit during his time.')
-('Louis', 'Richard', '1791', '1879', 'France', 'Long Nineteenth Century', 'Bronze Sculpting', 'Richard was a 19th century French scultor, known for creating realistic statues.')
+('Louis', 'Richard', '1791', '1879', 'France', 'Long Nineteenth Century', 'Bronze Sculpting', 'Richard was a 19th century French scultor, known for creating realistic statues.');
+('Louis', 'Barrias', '1841', '1905', 'France', 'Long Nineteenth Century', 'Romantic Realist', 'Barrias was a frech sculptor known for his romantic realist style and marble work.')
 
 INSERT INTO TABLE painting
 VALUES
 ('003', 'Oil', 'Panel', 'Renaissance')
 ('007', 'Oil', 'Canvas', 'Post-Impressionist')
-('006', 'Print', 'Transfer Lithograph', 'Post-Impressionist')
+('006', 'Print', 'Transfer Lithograph', 'Post-Impressionist');
+
+INSERT INTO TABLE other
+VALUES
+('002', 'Candelabrum', 'Rennaissance')
+('004', 'Ewer', 'Rennaissance')
+('005', 'Jug', 'Post-Modern');
+
+INSERT INTO TABLE statue
+VALUES
+
+INSERT INTO sculpture
+VALUES
+
 
