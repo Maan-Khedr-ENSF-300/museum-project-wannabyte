4cd3270e2b1c758bc82830e9c32dbd902cad9769
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2022-12-04 19:34:42 -0700

    nested retrieval query

--- SQL Scripts/art_museum_query.sql
+++ SQL Scripts/art_museum_query.sql
@@ -10,3 +10,16 @@ FROM painting;
 SELECT Lname, Fname, Year_born
 FROM artist 
 ORDER BY Year_born;
+
+-- Nested Retrieval Query
+SELECT Fname, Lname
+FROM artist
+WHERE Fname IN (
+	SELECT AFname
+    FROM art_object
+    WHERE ID_no IN (
+		SELECT ID_no
+        FROM painting
+        WHERE Paint_type = 'Oil'
+	)
+);
