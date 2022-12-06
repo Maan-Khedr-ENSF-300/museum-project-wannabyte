8512d778da0371f79baef55920be418fa9b6a27f
EadanL <113051274+EadanL@users.noreply.github.com>
2022-12-06 12:48:47 -0700

    resolving merging conflicts part 2

--- SQL Scripts/art_museum_query.sql
+++ SQL Scripts/art_museum_query.sql
@@ -32,14 +32,3 @@ AND o.ID_no = p.ID_no
 AND p.Paint_type = 'Oil';
 
 
--- Joined Table Query
-SELECT
-    borrowed.ID_no AS ID_no,
-    collections.CType AS Collection_type
-FROM borrowed
-JOIN collections ON borrowed.Borrowed_from = collections.CName;
-
-
--- Deletion Operation
-DELETE FROM sculpture 
-WHERE ID_no = '001';
